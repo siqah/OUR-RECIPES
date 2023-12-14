118110000e3925513fabcd2ca7a6579081494586
Siqah <faruoqmuhammed@zetech.ac.ke>
2023-12-14 18:34:50 +0300

    intiall

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
+import SearchBar from "./components/SearchBar";
+import RecipeCard from "./components/RecipeCard";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s="
+
+
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+
+  //function that fetch data from the api according to the query
+  
+
+  const  searchRecipes = async () => {
+    setIsLoading(true);
+    const url = apiUrl + query
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    setRecipes(data.meals);
+    setIsLoading(false)
+  };
+
+  useEffect(() => {
+     searchRecipes()
+     
+  }, [])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+       <h2>Our Recipe App </h2>
     </div>
   );
 }
 
 export default App;
+
