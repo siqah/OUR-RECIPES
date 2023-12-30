a0955e3a6c65492a0e2bf3ca6f74f883f777b67a
Siqah <faruoqmuhammed@zetech.ac.ke>
2023-12-30 09:08:38 +0300

    version 2.1

--- src/App.js
+++ src/App.js
@@ -2,37 +2,41 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import SearchBar from "./components/SearchBar";
 import RecipeCard from "./components/RecipeCard";
+import Spinner from "./components/Spinner"; // Import a spinner component
+
 const searchApi = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [query, setQuery] = useState("");
   const [recipes, setRecipes] = useState([]);
-  
+
   useEffect(() => {
     const searchRecipes = async () => {
       setIsLoading(true);
       const url = searchApi + query;
-      const res = await fetch(url);
-      const data = await res.json();
-      setRecipes(data.meals);
-      setIsLoading(false);
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        setRecipes(data.meals);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setIsLoading(false);
+      }
     };
 
-    // Invoke the searchRecipes function here
     searchRecipes();
   }, [query]);
 
   return (
     <div className="container">
       <h2>Our Food Recipes</h2>
-      <SearchBar
-        isLoading={isLoading}
-        query={query}
-        setQuery={setQuery}
-      />
+      <SearchBar isLoading={isLoading} query={query} setQuery={setQuery} />
       <div className="recipes">
-        {recipes ? (
+        {isLoading ? (
+          <Spinner /> // Use a spinner component for loading
+        ) : recipes && recipes.length > 0 ? (
           recipes.map((recipe) => (
             <RecipeCard key={recipe.idMeal} recipe={recipe} />
           ))
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+// Spinner.js
+import React from "react";
+import "./Spinner.css"; // Add CSS styles for the spinner
+
+const Spinner = () => {
+  return <div className="spinner"></div>;
+};
+
+export default Spinner;
